f6b807da56c8581219d2e362135f6a673b663d4b
Adrian Casillas <158532192+adriaaancg@users.noreply.github.com>
2025-05-31 20:52:02 -0600

    Delete backend directory

--- backend/controllers/dashboard.controller.js
+++
@@ -1,6 +0,0 @@
-exports.getDashboard = (req, res) => {
-  res.json({
-    message: "Dashboard data accessible",
-    user: req.user
-  });
-};
--- backend/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const dashboardController = require('../controllers/dashboard.controller');
-
-// Aquí solo /dashboard, porque en index.js tienes /api
-router.get('/dashboard', auth, dashboardController.getDashboard);
-
-module.exports = router;
